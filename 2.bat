19d23fa1b332e4f272a0fdaf65c34a151ef02bd9
gabrielpondc <gabrielpondc@yahoo.com>
2023-01-26 04:40:20 +0900

    'bk'

--- 2.bat
+++ 2.bat
@@ -4,4 +4,7 @@ for /f  %%i in ('dir /b /ad "%curdir%"') do (
 	Forfiles /p "%curdir%\%%i" /m *.nb3 /d -2 /c "cmd /c del @path -y"  
 	CHOICE /T 3 /C ync /CS /D y /n
 )
+git add .
+git commit -m 'bk'
+git push -u origin main
 exit
